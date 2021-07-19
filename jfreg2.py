8f0cfeac72f3c191139548d45293d4bd60a9d539
Jeff Engelmann <jme2041@icloud.com>
2021-07-19 01:13:28 -0500

    Implement field map preprocessing

--- jfreg2.py
+++ jfreg2.py
@@ -127,6 +127,21 @@ def main(argv):
         metavar='T1_HEAD',
         help='T1-weighted dataset in subject space (not brain-extracted)')
 
+    g1.add_argument('--fieldmap-rads',
+        required=True,
+        metavar='FM_RADS',
+        help='Field map in units of radians/second (phase-reconstructed)')
+
+    g1.add_argument('--fieldmap-mag-brain',
+        required=True,
+        metavar='FM_MAG_BRAIN',
+        help='Magnitude-reconstructed field map (brain-extracted)')
+
+    g1.add_argument('--fieldmap-mag-head',
+        required=True,
+        metavar='FM_MAG_HEAD',
+        help='Magnitude-reconstructed field map (not brain-extracted)')
+
     g2 = parser.add_argument_group('options')
 
     g2.add_argument('--standard-brain',
@@ -168,14 +183,49 @@ def main(argv):
             standard_brain)
     standard_brain = st2
 
+    print('jfreg2 begins....')
+
     # Look for input datasets
     t1_brain = os.path.abspath(opts.t1_brain)
     if not dset_exists(t1_brain):
         raise IOError('Could not find --t1-brain dataset: %s' % t1_brain)
+    print('--t1-brain: %s' % t1_brain)
 
     t1_head = os.path.abspath(opts.t1_head)
     if not dset_exists(t1_head):
         raise IOError('Could not find --t1-head dataset: %s' % t1_head)
+    print('--t1-head: %s' % t1_head)
+
+    fm_rads = os.path.abspath(opts.fieldmap_rads)
+    if not dset_exists(fm_rads):
+        raise IOError('Could not find --fieldmap-rads dataset: %s' % fm_rads)
+    print('--fieldmap-rads: %s' % fm_rads)
+
+    fm_mag_brain = os.path.abspath(opts.fieldmap_mag_brain)
+    if not dset_exists(fm_mag_brain):
+        raise IOError('Could not find --fieldmap-mag-brain dataset: %s' %
+            fm_mag_brain)
+    print('--fieldmap-mag-brain: %s' % fm_mag_brain)
+
+    fm_mag_head = os.path.abspath(opts.fieldmap_mag_head)
+    if not dset_exists(fm_mag_head):
+        raise IOError('Could not find --fieldmap-mag-head dataset: %s' %
+            fm_mag_head)
+    print('--fieldmap-mag-head: %s' % fm_mag_head)
+
+    # Warn if --fieldmap-mag-brain appears not to be brain-extracted
+    print('Checking fieldmap....')
+    capture = cmd('fslstats', fm_mag_brain, '-V', capture_output=True)
+    nonzero_voxels = float(capture.stdout.decode().split()[0])
+    print('Non-zero voxels: %d' % int(nonzero_voxels))
+    capture = cmd('fslstats', fm_mag_brain, '-v', capture_output=True)
+    total_voxels = float(capture.stdout.decode().split()[0])
+    print('Total voxels: %d' % int(total_voxels))
+    prop_nonzero = nonzero_voxels / total_voxels;
+    print('Proportion non-zero: %f' % prop_nonzero)
+    if(prop_nonzero > 0.9):
+        print('WARNING: High proportion of non-zero voxels in brain-extracted '
+            'field map')
 
     # Create output directory
     outdir = os.path.abspath(opts.outdir)
@@ -186,9 +236,154 @@ def main(argv):
             raise IOError('Output directory already exists: %s' % outdir)
     os.mkdir(outdir)
 
+    # ####################
+    # PREPROCESS FIELD MAP
+    # ####################
+
+    # These steps come from mainfeatreg's preprocessFieldmaps subroutine
+
+    print('Preprocessing fieldmap....')
+
+    fm_mag_brain_mask = os.path.join(outdir,
+        strip_ext(os.path.basename(fm_mag_brain)) + '_mask')
+    fm_mag_brain_mask_inv = fm_mag_brain_mask + '_inv'
+    fm_mag_brain_mask_idx = fm_mag_brain_mask + '_idx'
+    fm_mag_brain_mask50 = fm_mag_brain_mask + '50'
+    fm_mag_brain_mask_ero = fm_mag_brain_mask + '_ero'
+    fm_mag_brain_masked = fm_mag_brain_mask + 'ed'
+    fm_rads_brain = os.path.join(outdir,
+        strip_ext(os.path.basename(fm_rads)) + '_brain')
+    fm_rads_brain_tmp_fmapfilt = fm_rads_brain + '_tmp_fmapfilt'
+
+    cmd('fslmaths',
+        fm_mag_brain,
+        '-bin',
+        fm_mag_brain_mask,
+        '-odt', 'short')
+
+    cmd('fslmaths',
+        fm_rads,
+        '-abs',
+        '-bin',
+        '-mas', fm_mag_brain_mask,
+        '-mul', str(-1),
+        '-add', str(1),
+        '-bin',
+        fm_mag_brain_mask_inv)
+
+    cmd('cluster',
+        '-i', fm_mag_brain_mask_inv,
+        '-t', str(0.5),
+        '--no_table',
+        '-o', fm_mag_brain_mask_idx)
+
+    capture = cmd('fslstats',
+        fm_mag_brain_mask_idx,
+        '-R',
+        capture_output=True)
+    maxidx = float(capture.stdout.decode().split()[1])
+
+    cmd('fslmaths',
+        fm_mag_brain_mask_idx,
+        '-thr', str(maxidx),
+        '-bin',
+        '-mul', str(-1),
+        '-add', str(1),
+        '-bin',
+        '-mas', fm_mag_brain_mask,
+        fm_mag_brain_mask)
+
+    capture = cmd('fslstats',
+        fm_rads,
+        '-k', fm_mag_brain_mask,
+        '-P', str(50),
+        capture_output=True)
+    mean = float(capture.stdout.decode())
+
+    cmd('fslmaths',
+        fm_rads,
+        '-sub', str(mean),
+        '-mas', fm_mag_brain_mask,
+        fm_rads_brain)
+
+    capture = cmd('fslstats',
+        fm_mag_brain,
+        '-P', str(98),
+        capture_output=True)
+    thresh50 = float(capture.stdout.decode())/2.0
+
+    cmd('fslmaths',
+        fm_mag_brain,
+        '-thr', str(thresh50),
+        fm_mag_brain_mask50)
+
+    cmd('fslmaths',
+        fm_mag_brain_mask,
+        '-ero',
+        fm_mag_brain_mask_ero)
+
+    cmd('fslmaths',
+        fm_mag_brain_mask_ero,
+        '-add', fm_mag_brain_mask50,
+        '-thr', str(0.5),
+        '-bin',
+        fm_mag_brain_mask)
+
+    cmd('fslmaths',
+        fm_rads_brain,
+        '-mas', fm_mag_brain_mask,
+        fm_rads_brain)
+
+    cmd('fslmaths',
+        fm_mag_brain,
+        '-mas', fm_mag_brain_mask,
+        fm_mag_brain_masked)
+
+    cmd('fslmaths',
+        fm_mag_brain_mask,
+        '-ero',
+        fm_mag_brain_mask_ero)
+
+    cmd('fugue',
+        '--loadfmap=%s' % fm_rads_brain,
+        '--savefmap=%s' % fm_rads_brain_tmp_fmapfilt,
+        '--mask=%s' % fm_mag_brain_mask,
+        '--despike',
+        '--despikethreshold=2.1')
+
+    cmd('fslmaths',
+        fm_rads_brain,
+        '-sub', fm_rads_brain_tmp_fmapfilt,
+        '-mas', fm_mag_brain_mask_ero,
+        '-add', fm_rads_brain_tmp_fmapfilt,
+        fm_rads_brain)
+
+    capture = cmd('fslstats',
+        fm_rads_brain,
+        '-k', fm_mag_brain_mask,
+        '-P', str(50),
+        capture_output=True)
+    median = float(capture.stdout.decode())
+
+    cmd('fslmaths',
+        fm_rads_brain,
+        '-sub', str(median),
+        '-mas', fm_mag_brain_mask,
+        fm_rads_brain)
+
+    cmd('imrm', fm_rads_brain_tmp_fmapfilt)
+    cmd('imrm', fm_mag_brain_mask_ero)
+    cmd('imrm', fm_mag_brain_mask50)
+    cmd('imrm', fm_mag_brain_mask_idx)
+    cmd('imrm', fm_mag_brain_mask_inv)
+
+    return
+
     # ################################################
     # SPATIAL NORMALIZATION OF THE T1-WEIGHTED DATASET
     # ################################################
+
+    print('Spatially normalizing T1-weighted dataset....')
 
     t1_brain_to_standard_dset = os.path.join(outdir,
         strip_ext(os.path.basename(t1_brain)) + '_to_standard')
